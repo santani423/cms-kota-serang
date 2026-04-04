77f04497273425caad523aa1b676c7d3e9ecc5b2
santani423 <santani423@gmail.com>
2026-04-05 00:13:01 +0700

    cormenu

--- app/Http/Controllers/Api/SettingsController.php
+++ app/Http/Controllers/Api/SettingsController.php
@@ -9,12 +9,100 @@ class SettingsController extends Controller
 {
     public function cormenu(Request $request)
     {
-        // Contoh data statis, bisa diganti dengan data dari database
         $data = [
-            'menu' => [
-                ['id' => 1, 'name' => 'Dashboard', 'url' => '/dashboard'],
-                ['id' => 2, 'name' => 'Users', 'url' => '/users'],
-                ['id' => 3, 'name' => 'Settings', 'url' => '/settings'],
+            [
+                "code" => "beranda",
+                "title" => "Beranda",
+                "href" => "/",
+            ],
+            [
+                "code" => "layanan-publik",
+                "title" => "Layanan Publik",
+                "href" => "/public-services",
+            ],
+            [
+                "code" => "profile",
+                "title" => "Profil",
+                "href" => "/informasi-publik",
+                "subMenu" => [
+                    [
+                        "title" => "Walikota Serang",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Wakil Walikota Serang",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Visi Misi",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Sejarah Kota Serang",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Arti Lambang Kota Serang",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Penghargaan",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Latar Geografi",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "",
+                    ],
+                    [
+                        "title" => "Pejabat Kota Serang",
+                        "href" => "/leadership",
+                        "desc" => "",
+                    ],
+                ],
+            ],
+            [
+                "code" => "informasi-publik",
+                "title" => "Informasi Publik",
+                "href" => "/informasi-publik",
+                "subMenu" => [
+                    [
+                        "title" => "Profil Pemerintah",
+                        "href" => "/informasi-publik/profil",
+                        "desc" => "Informasi struktur dan profil pemerintah daerah",
+                    ],
+                    [
+                        "title" => "Visi & Misi",
+                        "href" => "/informasi-publik/visi-misi",
+                        "desc" => "Arah pembangunan dan tujuan daerah",
+                    ],
+                    [
+                        "title" => "Dokumen Publik",
+                        "href" => "/informasi-publik/dokumen",
+                        "desc" => "Akses dokumen resmi dan laporan",
+                    ],
+                    [
+                        "title" => "PPID",
+                        "href" => "/informasi-publik/ppid",
+                        "desc" => "Pejabat Pengelola Informasi dan Dokumentasi",
+                    ],
+                ],
+            ],
+            [
+                "code" => "berita",
+                "title" => "Berita",
+                "href" => "/news",
+            ],
+            [
+                "code" => "wisata",
+                "title" => "Wisata",
+                "href" => "/tourism",
             ],
         ];
 
