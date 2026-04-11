ffac7cf9ddd5386c5d3819aa10b8b2f78faaa3f4
santani423 <santani423@gmail.com>
2026-04-11 23:20:56 +0700

    add massage

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\MassageSupport;
 
 class SupportController extends Controller
 {
     public function store(Request $request)
     {
-        return response()->json(['message' => 'Support request received successfully.'], 200);
         $request->validate([
             'email' => 'required|string|max:255',
             'hp' => 'nullable|string|max:255',
             'message' => 'required|string',
         ]);
+        return response()->json(['message' => 'Support request received successfully.'], 200);
 
         // Here you can handle the support request, e.g., save it to the database or send an email
-        $support = new \App\Models\MassageSupport();
+        $support = new MassageSupport();
         $support->email = $request->email;
         $support->hp = $request->hp;
         $support->message = $request->message;
